3dfd9b28c5f52ee45158b21590cacd8f94afe169
alan.lucki@gmail.com <alan.lucki@gmail.com>
2022-10-06 08:56:16 -0500

    22/10/06-08:56:10

---
+++ [ 01 ] introduccion/[ 05 ] comentarios.sh
@@ -0,0 +1,12 @@
+: '  
+este es un comentarios multilinea
+este es un comentarios multilinea
+este es un comentarios multilinea
+este es un comentarios multilinea
+este es un comentarios multilinea
+'
+
+echo "comentarios multilinea"
+
+#Este es un comentario
+echo "comentario de una linea"
